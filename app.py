98e203885b19c95e445371dda31427f7dd7b9182
Alex-ast7 <81142538+Alex-ast7@users.noreply.github.com>
2024-06-24 12:50:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 from difflib import ndiff
 from annotated_text import annotated_text
 from optimum.intel import OVModelForSeq2SeqLM
-
+from Levenshtein import editops
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 @st.cache_resource
 def get_sage():
     tokenizer = AutoTokenizer.from_pretrained("ai-forever/sage-fredt5-distilled-95m")
     #model = AutoModelForSeq2SeqLM.from_pretrained("ai-forever/sage-fredt5-distilled-95m")
-    model = OVModelForSeq2SeqLM.from_pretrained('app/sage-t5-small-ov-f32', local_files_only=True, compile=True)
+    model = OVModelForSeq2SeqLM.from_pretrained('app/', local_files_only=True, compile=True)
     #model.to("cuda")
     return tokenizer, model
 
@@ -21,25 +21,46 @@ def highlight_differences(original, corrected):
     # print(list(diff))
     original = original.split()
     corrected = corrected.split()
+
     for word in range(len(corrected)):
         error = []
-        if len(corrected[word]) > len(original[word]):
-          if corrected[word][-1] in ",.:;'[]|\()!@#$%^&*-=+?" and corrected[word][-1] != original[word][-1]:
-            error.append('пропущен знак препинания')
-            if len(corrected[word][:-1]) > len(original[word]):
-              error.append('пропущена буква')
-          else:
-              error.append('пропущена буква')
-        if len(corrected[word]) < len(original[word]):
-          error.append('лишняя буква')
-        n = min(len(corrected[word]), len(original[word]))
-        for i in range(n):
-            print(corrected[word][i], original[word][i])
-            if corrected[word][i].islower() != original[word][i].islower() and 'регистр буквы' not in error:
-                error.append('регистр буквы')
+        dif = editops(original[word], corrected[word])
+        for type in dif:
+            ind1 = type[1]
+            ind2 = type[2]
+            if corrected[word][ind2] in ",.:;'[]|\()!@#$%^&*-=+?" and 'пропущен знак препинания' not in error:
+                error.append('пропущен знак препинания')
+            else:
+                if type[0] == 'replace' and corrected[word][ind2].islower() != original[word][ind1].islower() and original[word][ind1] not in ",.:;'[]|\()!@#$%^&*-=+?" and 'регистр буквы' not in error:
+                    error.append('регистр буквы')
+                elif type[0] == 'replace' and 'ошибка в написании' not in error:
+                    error.append('ошибка в написании')
+                if type[0] == 'insert' and corrected[word][ind2] in ",.:;'[]|\()!@#$%^&*-=+?" and 'пропущен знак препинания' not in error:
+                    error.append('пропущен знак препинания')
+                elif type[0] == 'insert' and 'пропущена буква' not in error:
+                    error.append('пропущена буква')
+                if type[0] == 'delete' and 'лишняя буква' not in error:
+                    error.append('лишняя буква')
 
-            if corrected[word][i].lower() != original[word][i].lower() and 'ошибка в написании' not in error:
-                error.append('ошибка в написании')
+
+
+        # if len(corrected[word]) > len(original[word]):
+        #   if corrected[word][-1] in ",.:;'[]|\()!@#$%^&*-=+?" and corrected[word][-1] != original[word][-1]:
+        #     error.append('пропущен знак препинания')
+        #     if len(corrected[word][:-1]) > len(original[word]):
+        #       error.append('пропущена буква')
+        #   else:
+        #       error.append('пропущена буква')
+        # if len(corrected[word]) < len(original[word]):
+        #   error.append('лишняя буква')
+        # n = min(len(corrected[word]), len(original[word]))
+        # for i in range(n):
+        #     print(corrected[word][i], original[word][i])
+        #     if corrected[word][i].islower() != original[word][i].islower() and 'регистр буквы' not in error:
+        #         error.append('регистр буквы')
+        #
+        #     if corrected[word][i].lower() != original[word][i].lower() and 'ошибка в написании' not in error:
+        #         error.append('ошибка в написании')
 
         if error:
             if on:
